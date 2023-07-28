008400a355eb2aaeb1c044ba0a2204f05120a193
Martha Nava Hernández <78755615+MarthaNH@users.noreply.github.com>
2023-07-27 23:34:01 -0600

    Add files via upload

---
+++ Sesión-01/Reto-01.js
@@ -0,0 +1,20 @@
+
+var a = 5;
+var b = 10;
+var c = 15;
+
+
+const1 = (3 + b) * c / a * 2;
+const2 = ((a + b * c) / 5) * 2;
+const3 = b / a + 2 *c;
+const4 = (a + b + c / c) * a;
+const5 = ((3 * a) / c) + a + b + c;
+const6 = a - (b + c) * a /1;
+
+
+console.log(const1)
+console.log(const2)
+console.log(const3)
+console.log(const4)
+console.log(const5)
+console.log(const6)
