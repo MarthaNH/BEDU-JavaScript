03813448bb99093afb7f0ae72708c9c1d447fd3b
Martha Nava Hernández <78755615+MarthaNH@users.noreply.github.com>
2023-07-27 23:49:19 -0600

    Update Ejemplo-03.js

--- Sesión-02/Ejemplo-03.js
+++ Sesión-02/Ejemplo-03.js
@@ -1,1 +1,15 @@
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
 
+//Excelente, funcionará y se reflejarán en la consola.
+
+//Ahora bien, si te dijera que queremos repetirlo 200 veces, entonces copiar y pegar cada línea se volvería ineficiente ( aparte de odiar al que te dio la instrucción).
+
+//¿Qué podemos hacer?
+
+for(let i = 0; i<=200; i++){
+  console.log("Hello World");
+}
