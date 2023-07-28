97705d63d6f59b2d4c82bc4c8ebb2c07ad634f63
Martha Nava Hernández <78755615+MarthaNH@users.noreply.github.com>
2023-07-27 23:45:43 -0600

    Update Ejemplo-01.js

--- Sesión-02/Ejemplo-01.js
+++ Sesión-02/Ejemplo-01.js
@@ -1,1 +1,10 @@
+const time = 13;
+let greeting;
 
+if (time < 12) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good afternoon";
+} else if (time >= 20) {
+  greeting = "Good evening";
+}
