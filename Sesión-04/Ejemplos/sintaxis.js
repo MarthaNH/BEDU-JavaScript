be124a6979ead405946195bbcf16253fe317350a
Martha Nava Hernández <78755615+MarthaNH@users.noreply.github.com>
2023-08-07 23:36:37 -0600

    Add files via upload

---
+++ Sesión-04/Ejemplos/sintaxis.js
@@ -0,0 +1,15 @@
+const firstNames = [ 'John', 'Jane', 'Mark'];
+
+const getFullNames = (names) => {
+  const fullNames = []
+	
+  for(const name of names) {
+    fullNames.push(`${name} Doe`)
+  }
+  
+  return fullNames
+}
+
+const fullNames = getFullNames(firstNames)
+
+console.log(fullNames); // ["John Doe", "Jane Doe", "Mark Doe"]
